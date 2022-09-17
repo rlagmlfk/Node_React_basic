41b0e26101b227bcc9fe2103ab9d5fe0a5f3552e
rlagmlfk <gmlfk703@gmail.com>
2022-09-18 00:06:18 +0900

    Bcrypt로 비밀번호 암호화하기

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.post('/register', (req, res) => {
 
 
     const user = new User(req.body)
-
     user.save((err, userInfo) => {
         if (err) return res.json({ success: false, err })
         return res.status(200).json({
