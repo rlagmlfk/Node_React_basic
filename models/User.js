afa0888fe13faf56509e046a78c4c35d06dedd85
rlagmlfk <gmlfk703@gmail.com>
2022-09-25 00:11:18 +0900

    로그인 기능 with Bcrypt and 토큰생성 with jsonwebtoken

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -47,9 +48,37 @@ userSchema.pre('save', function (next) {
                 next()
             });
         });
+    } else {
+        next()
     }
 })
 
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    
+    // plainPassword 1234567 == 암호화된 비밀번호 $2b$10$U8wbGKBJBLsFO0txBaQruu.BgMhp9nXNEJxmCarjobiVcaqD.6EFu
+    // 암호화된 비밀번호를 복호화할 수 없기때문에 plainPassword를 암호화하여 비교한다.
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return (err),
+        cb(null, isMatch)
+    })
+}
+userSchema.methods.generateToken = function (cb) {
+
+    let user = this;
+
+    // jsonwebtoken을 이용해서 token 생성하기
+    
+    let token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    //user._id + 'secretToken' = token
+    //-> 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
 const User = mongoose.model('User', userSchema)
 
 module.exports = {User}
