86fd1a789028162f11549437ec773d0e3076b425
rlagmlfk <gmlfk703@gmail.com>
2022-10-08 22:12:29 +0900

    로그인 페이지

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { loginUser } from '../../../_actions/user_action';
+
 
 function LoginPage(props) {
+
+    const dispatch = useDispatch();
+    let navigate = useNavigate();    
+    // state 데이터의 변화를 시켜줌
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+    const onSubmitHandler = (event) => {
+        event.preventDefault(); // 페이지의 리프레쉬를 막아줌
+        // console.log('Email', Email)
+        // console.log('Password', Password)
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+            if (response.payload.loginSuccess) {
+                // 버전문제로 props.history.push('/')를 쓰면
+                // TypeError: Cannot read properties of undefined (reading 'push')에러가 남
+                // useNavigate를 사용해 root페이지로 이동
+                navigate('/') 
+            } else {
+                alert('Error')
+            }
+        })
+
+    }
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            ,width:'100%', height:'100vh'
+        }}>
+            <form style={{ display:'flex', flexDirection:'column' }} onSubmit={onSubmitHandler}>
+                <label>Email</label> <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label> <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button style={{}}>
+                    Login
+                </button>
+            </form>
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.post('/api/user/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/user/login', (req, res) => {
     
     // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
 
