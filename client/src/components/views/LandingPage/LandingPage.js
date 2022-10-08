d4492aa3567e16e5b58dd888ba796c3c9bfb3a04
rlagmlfk <gmlfk703@gmail.com>
2022-10-08 22:49:10 +0900

    로그아웃

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,35 @@
 import React, {useEffect} from 'react';
 import axiox from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage(props) {
+
+    let navigate = useNavigate(); 
     
     useEffect(() => {
         axiox.get('/api/hello')
         .then(response => console.log(response.data))
     }, [])
 
+    const onClickHandler = () => {
+        axiox.get('/api/users/logout')
+            .then(response => {
+            if (response.data.success) {
+                navigate('/login')
+            } else {
+                alert('로그아웃 실패')
+            }
+        })
+    }
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center'
             ,width:'100%', height:'100vh'
         }}>
             <h2>시작 페이지</h2>
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     );
 }
